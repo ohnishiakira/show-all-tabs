70487132fefdefd7325ec981ac4decd7d9eaa0a6
Ohnishi Akira <s06206ao@gmail.com>
2011-02-08 06:32:35 +0900

    Second commit.

---
+++ showalltabs.js
@@ -0,0 +1,38 @@
+function movetab() {
+  var tabId = parseInt(this.id.substring(1), 10);
+  chrome.tabs.update(tabId, {"selected":true});
+}
+
+window.onload = function() {
+  var currentTab = null;
+  chrome.tabs.getSelected(null, function(tab) {
+    currentTab = tab;
+  });
+
+  var ul = document.querySelector("ul");
+
+  chrome.tabs.getAllInWindow(null, function(tabs) {
+    for (var i = 0; i < tabs.length; i++) {
+      var li = document.createElement("li");
+      var favicon = document.createElement("img");
+      var title = document.createElement("span");
+
+      favicon.setAttribute("class", "favicon");
+      favicon.setAttribute("src", tabs[i].favIconUrl);
+
+      title.setAttribute("class", "title");
+      title.innerText = tabs[i].title;
+
+      if (currentTab.url === tabs[i].url) {
+        li.setAttribute("class", "current");
+      }
+      li.setAttribute("id", "t" + tabs[i].id);
+      li.setAttribute("title", tabs[i].url);
+      li.appendChild(favicon);
+      li.appendChild(title);
+      li.addEventListener("click", movetab, false);
+
+      ul.appendChild(li);
+    }
+  });
+};
